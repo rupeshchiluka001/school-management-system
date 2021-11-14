afa6f194b773d0ffb88dbbe4c9596a720caa62f4
Rupesh Chiluka <rupeshchiluka@gmail.com>
2021-11-14 16:58:20 +0530

    Added Div for book card

--- views/src/app/library/book/book.component.ts
+++ views/src/app/library/book/book.component.ts
@@ -1,26 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
-  @Input() title!: string;
-  @Input() author!: string;
-  @Input() field!: string;
-  @Input() category!: string;
-  @Input() description!: string;
-  @Input() stock!: Number;
+  @Input() book!: Book;
   currentlyAvailable: Boolean = false;
+  divShown = false;
+  @ViewChild('bookOp') bookOp!: ElementRef;
+  div!: HTMLDivElement;
 
   ngOnInit(): void {
-    if (this.stock > 0) {
+    if (this.book.stock > 0) {
       this.currentlyAvailable = true;
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.div = this.renderer.selectRootElement(this.bookOp["nativeElement"], true);
+  }
+
+  toggleDiv(): void {
+    this.divShown = !this.divShown;
+    this.div.style.visibility = (this.divShown) ? 'visible' : 'hidden';
   }
 
 }
